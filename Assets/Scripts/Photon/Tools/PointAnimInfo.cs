5391085b83316beffd41c7b000249a70172fda52
PLSAM <samarachtingi@gmail.com>
2019-12-04 19:05:58 +0100

    debut point feed back

---
+++ Assets/Scripts/Photon/Tools/PointAnimInfo.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointAnimInfo : MonoBehaviour
+{
+    public Text myText;
+
+    public void SetColor(Color color, int value)
+    {
+        myText.color = color;
+        myText.text = value.ToString();
+
+        StartCoroutine(DestroyObj());
+    }
+
+    IEnumerator DestroyObj()
+    {
+        yield return new WaitForSeconds(5);
+        PhotonNetwork.Destroy(GetComponent<PhotonView>());
+    }
+}
