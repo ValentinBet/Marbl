c0b08bcaecd9afd84db7d6fe7f617c9b4194334f
PLSAM <samarachtingi@gmail.com>
2019-12-05 19:17:52 +0100

    Loading panel

---
+++ Assets/Scripts/UI/PlayerListElement.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerListElement : MonoBehaviour
+{
+    public Player myPlayer;
+
+    public Text nameText;
+    public Text statutText;
+}
