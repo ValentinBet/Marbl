c5eef055d2d64a1037d16621a58e8573ba884daf
PLSAM <samarachtingi@gmail.com>
2019-12-09 13:53:28 +0100

    system de fondu d obstacle

---
+++ Assets/Materials/Shaders/Transparence.shader
@@ -0,0 +1,65 @@
+﻿Shader "Custom/Transparent"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Alpha("Alpha", Range(0, 1)) = 1
+	}
+		SubShader
+		{
+			Tags
+			{
+				"RenderType" = "Transparent"
+				"IgnoreProjector" = "True"
+				"Queue" = "Transparent"
+			}
+
+			LOD 200
+
+			Pass { ColorMask 0 }
+			ZWrite Off
+			Alphatest Greater 0
+			Blend SrcAlpha OneMinusSrcAlpha
+			ColorMask RGB
+
+			CGPROGRAM
+
+			#pragma surface surf Standard addshadow novertexlights noforwardadd alpha:fade
+
+			// Use shader model 3.0 target, to get nicer looking lighting
+			#pragma target 3.0
+
+			struct Input
+			{
+				float2 uv_MainTex;
+				float3 worldPos;
+				float4 color : COLOR;
+			};
+
+			fixed4 _Color;
+			sampler2D _MainTex;
+			half _Glossiness;
+			float _Alpha;
+
+			UNITY_INSTANCING_BUFFER_START(Props)
+				// put more per-instance properties here
+			UNITY_INSTANCING_BUFFER_END(Props)
+
+			void surf(Input IN, inout SurfaceOutputStandard o)
+			{
+				fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * IN.color * _Color;
+				o.Albedo = c.rgb;
+				o.Smoothness = _Glossiness;
+
+				// Fade alpha depending on camera position
+				float dist = distance(IN.worldPos, _WorldSpaceCameraPos);
+				c.a = _Alpha;
+
+				o.Alpha = c.a;
+			}
+			ENDCG
+		}
+			FallBack "Diffuse"
+}
---
+++ Assets/Scripts/Tool/HideObstacle.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObstacle : MonoBehaviour
+{
+
+    public LayerMask ObsAndBall;
+
+    GameObject objHide;
+
+    List<Renderer> oldObs = new List<Renderer>();
+
+    float start;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray forwardRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+
+        if (Physics.Raycast(forwardRay, out hit, ObsAndBall))
+        {
+            if(hit.transform.gameObject.layer == 14)
+            {
+                if(hit.transform.gameObject != objHide)
+                {
+                    objHide = hit.transform.gameObject;
+                    start = objHide.GetComponent<Renderer>().material.GetFloat("_Alpha");
+
+                }
+                else
+                {
+                    start = Mathf.Lerp(start, 0.2f, 3 * Time.deltaTime);
+                    objHide.GetComponent<Renderer>().material.SetFloat("_Alpha", start);
+                }
+            }
+            else
+            {
+                if (objHide != null && !oldObs.Contains(objHide.GetComponent<Renderer>()))
+                {
+                    oldObs.Add(objHide.GetComponent<Renderer>());
+                }
+                objHide = null;
+            }
+        }
+        else
+        {
+            if (objHide != null && !oldObs.Contains(objHide.GetComponent<Renderer>()))
+            {
+                oldObs.Add(objHide.GetComponent<Renderer>());
+            }
+            objHide = null;
+        }
+
+
+        List<Renderer> removeThis = new List<Renderer>();
+        foreach(Renderer element in oldObs)
+        {
+            start = Mathf.Lerp(start, 1f, 3 * Time.deltaTime);
+            element.material.SetFloat("_Alpha", start);
+
+            if(start > 0.99f)
+            {
+                removeThis.Add(element);
+            }
+        }
+
+        foreach(Renderer element in removeThis)
+        {
+            oldObs.Remove(element);
+        }
+    }
+}
