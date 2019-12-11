e0a5a7927cad172c7668272859df767eb096bdd8
PLSAM <samarachtingi@gmail.com>
2019-12-11 11:58:47 +0100

    avancement feedback points

---
+++ Assets/Scripts/UI/TeamListElement.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Photon.Pun.UtilityScripts.PunTeams;
+
+public class TeamListElement : MonoBehaviour
+{
+    public Team myTeam;
+
+    public Text teamText;
+    public Text pointText;
+    public Text marblNumber;
+
+
+    public void SetElements(Team team, string point, string marbl)
+    {
+        myTeam = team;
+
+        /*
+        switch (myTeam)
+        {
+            case 0:
+                teamText.text = "Team red";
+                break;
+
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+        }
+
+        //teamText.color = MarblGame.GetColor(myTeam);
+        */
+    }
+}
