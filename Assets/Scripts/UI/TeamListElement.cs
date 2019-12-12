41fe2416b2352ce6d742476fad07481f0d900415
PLSAM <samarachtingi@gmail.com>
2019-12-12 17:23:53 +0100

    new quickscoreboard

--- Assets/Scripts/UI/TeamListElement.cs
+++ Assets/Scripts/UI/TeamListElement.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using static Photon.Pun.UtilityScripts.PunTeams;
@@ -10,8 +11,13 @@ public class TeamListElement : MonoBehaviour
     public Team myTeam;
 
     public Text teamText;
-    public Text pointText;
+    public TextMeshProUGUI pointText;
     public Text marblNumber;
+
+    public List<Image> colorBackgroundObj;
+
+    float oldPoint = 0;
+    float newPoint = 0;
 
 
     public void SetElements(Team team, int point, int marbl)
@@ -21,8 +27,19 @@ public class TeamListElement : MonoBehaviour
         teamText.text = MarblGame.GetTeamString((int) myTeam);
         teamText.color = MarblGame.GetColor((int) myTeam);
 
-        pointText.text = point.ToString() + " pts.";
+        newPoint = point;
 
         marblNumber.text = marbl.ToString();
+
+        foreach(Image element in colorBackgroundObj)
+        {
+            element.color = MarblGame.GetColor((int)myTeam);
+        }
+    }
+
+    private void Update()
+    {
+        oldPoint = Mathf.Lerp(oldPoint, newPoint, 3 * Time.deltaTime);
+        pointText.text = Mathf.RoundToInt(oldPoint).ToString() + " pts.";
     }
 }
