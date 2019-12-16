df5bcbec2a1a8492509119846aa1e7a41b3813ab
PLSAM <samarachtingi@gmail.com>
2019-12-16 19:17:05 +0100

    new lobby WIP

---
+++ Assets/Scripts/UI/GamemodeElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamemodeElement : MonoBehaviour
+{
+    public Image Icon;
+    public Text labelMode;
+    public Text Description;
+    public int indexMode;
+
+    
+}
