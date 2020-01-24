b508054141e819162fc4010af9ab11514235734a
PLSAM <samarachtingi@gmail.com>
2020-01-24 10:16:08 +0100

    fix taille ping  + lancement sans choix de map

--- Assets/Scripts/Photon/Lobby/LobbyMainPanel.cs
+++ Assets/Scripts/Photon/Lobby/LobbyMainPanel.cs
@@ -162,6 +162,8 @@ public class LobbyMainPanel : MonoBehaviourPunCallbacks
     {
         SetActivePanel(InsideRoomPanel.name);
 
+        PhotonNetwork.CurrentRoom.SetMap(RoomScripts.Instance.map);
+
         GameObject playerChat = PhotonNetwork.Instantiate("PlayerChat", Vector3.zero, Quaternion.identity);
 
         ChatManager.Instance.lobbyPlayerChat = playerChat.GetComponent<LobbyChat>();
