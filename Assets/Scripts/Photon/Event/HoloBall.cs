91cef7b06603787871e7001bb427cf308d9e3013
PLSAM <samarachtingi@gmail.com>
2020-02-11 23:33:52 +0100

    holo test

---
+++ Assets/Scripts/Photon/Event/HoloBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloBall : MonoBehaviour
+{
+    Renderer myRenderer;
+    public Animator myAnimator;
+
+    private void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        myAnimator.SetBool("Blink", true);
+        if (other.tag != "Ball")
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        myAnimator.SetBool("Blink", false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        myAnimator.SetBool("Blink", true);
+    }
+}
