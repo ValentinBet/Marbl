53481edd5aa8de12c614f7fd4f7447f85433ecdb
pieR <p.Soriot@rubika-edu.com>
2020-02-17 15:21:13 +0100

    editeur bille changement de couleur bugged

--- Assets/Scripts/Pierre/SkinManager.cs
+++ Assets/Scripts/Pierre/SkinManager.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class SkinManager : MonoBehaviour
 {
     public static SkinManager instance;
-    [SerializeField] MeshRenderer marble;
 
+    [Header("Preview")]
+    [SerializeField] MeshRenderer marble;
+    [SerializeField] ParticleSystem changeFeedback;
+    Material currentMaterial;
+    Material oldMaterial;
+
+    [Header("PlayerInfo")]
+    [SerializeField] Sc_Skininfos playerSkinInfos;
 
     private void Awake()
     {
@@ -14,12 +21,46 @@ public class SkinManager : MonoBehaviour
             instance = this;
         else
             Destroy(this);
+
+
+        marble.material = playerSkinInfos.playerSkin;
     }
 
-   void UpdatePreview(Texture2D newText, Color colorOfTheLogo)
+   public void UpdatePreview(Material newMaterial)
    {
-        Material updatedMaterial;
-        //updatedMaterial.SetColor()
-    //    marble.material = updatedMaterial;
+        Material prepMaterial = new Material(newMaterial);
+
+        currentMaterial = newMaterial;
+        marble.material = newMaterial;
+        changeFeedback.Play();
    }
+
+    public void SetRed()
+    {
+        marble.material.SetColor("_MarbleColor",new Color(255,0,0,255));
+    }
+    public void SetBlue()
+    {
+        marble.material.SetColor("_MarbleColor", new Color(0, 184, 255, 255));
+    }
+    public void SetGreen()
+    {
+        marble.material.SetColor("_MarbleColor", new Color(0, 255, 0, 255));
+    }
+    public void SetYellow()
+    {
+        marble.material.SetColor("_MarbleColor", new Color(255, 226, 0, 255));
+    }
+
+    public void SetNewMaterial()
+    {
+        playerSkinInfos.playerSkin = currentMaterial;
+    }
+
+    public void Cancel()
+    {
+        playerSkinInfos.playerSkin = oldMaterial;
+        UpdatePreview(oldMaterial);
+        changeFeedback.Play();
+    }
 }
