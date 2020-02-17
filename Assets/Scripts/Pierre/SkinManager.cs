c730e3c3b9069221c3ee8408ca294aae62d54d6c
pieR <p.Soriot@rubika-edu.com>
2020-02-17 16:23:14 +0100

    editeur quasi fini

--- Assets/Scripts/Pierre/SkinManager.cs
+++ Assets/Scripts/Pierre/SkinManager.cs
@@ -11,6 +11,7 @@ public class SkinManager : MonoBehaviour
     [SerializeField] ParticleSystem changeFeedback;
     Material currentMaterial;
     Material oldMaterial;
+    ColorType colorType;
 
     [Header("PlayerInfo")]
     [SerializeField] Sc_Skininfos playerSkinInfos;
@@ -22,38 +23,57 @@ public class SkinManager : MonoBehaviour
         else
             Destroy(this);
 
-
+        oldMaterial = playerSkinInfos.playerSkin;
         marble.material = playerSkinInfos.playerSkin;
+        SetRed();
     }
 
-   public void UpdatePreview(Material newMaterial)
-   {
-        Material prepMaterial = new Material(newMaterial);
-
+    public void UpdatePreview(Material newMaterial)
+    {
         currentMaterial = newMaterial;
         marble.material = newMaterial;
+        switch (colorType)
+        {
+            case ColorType.Red:
+                SetRed();
+                break;
+            case ColorType.Blue:
+                SetBlue();
+                break;
+            case ColorType.Green:
+                SetGreen();
+                break;
+            case ColorType.Yellow:
+                SetYellow();
+                break;
+        }
         changeFeedback.Play();
-   }
+    }
 
     public void SetRed()
     {
-        marble.material.SetColor("_MarbleColor",new Color(255,0,0,255));
+        marble.material.SetColor("_MarbleColor", new Color(1, 0, 0, 1));
+        colorType = ColorType.Red;
     }
     public void SetBlue()
     {
-        marble.material.SetColor("_MarbleColor", new Color(0, 184, 255, 255));
+        marble.material.SetColor("_MarbleColor", new  Color(0, 0.72f, 1, 1));
+        colorType = ColorType.Blue;
     }
     public void SetGreen()
     {
-        marble.material.SetColor("_MarbleColor", new Color(0, 255, 0, 255));
+        marble.material.SetColor("_MarbleColor", new Color(0, 1, 0, 1));
+        colorType = ColorType.Green;
     }
     public void SetYellow()
     {
-        marble.material.SetColor("_MarbleColor", new Color(255, 226, 0, 255));
+        marble.material.SetColor("_MarbleColor", new Color(1, .88f, 0, 1));
+        colorType = ColorType.Yellow;
     }
 
     public void SetNewMaterial()
     {
+        oldMaterial = currentMaterial;
         playerSkinInfos.playerSkin = currentMaterial;
     }
 
@@ -62,5 +82,10 @@ public class SkinManager : MonoBehaviour
         playerSkinInfos.playerSkin = oldMaterial;
         UpdatePreview(oldMaterial);
         changeFeedback.Play();
+    }
+
+    public enum ColorType
+    {
+        Red, Yellow,Blue,Green
     }
 }
