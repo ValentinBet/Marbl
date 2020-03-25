b99231271a91e5832dba34b112650e26e02ea001
pier <p.Soriot@rubika-edu.com>
2020-03-25 15:08:59 +0100

    complement scene de skins

---
+++ Assets/Scripts/Pierre/Mb_MarbleRot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Mb_MarbleRot : MonoBehaviour
+{
+    CinemachineOrbitalTransposer cot;
+    [SerializeField] CinemachineVirtualCamera CVC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cot = CVC.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            cot.m_XAxis.m_MaxSpeed = 300.0f;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            cot.m_XAxis.m_MaxSpeed = 0.0f;
+        }
+    }
+}
